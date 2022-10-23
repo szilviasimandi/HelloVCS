13d28635608b9f68e44dfd4067acad5b55da051c
simandiszilvia <simandiszilvia@K00133448>
2022-10-23 18:48:00 +0200

    Add project files.

---
+++ HelloVCS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloVCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, VCS!");
+        }
+
+    }
+}
